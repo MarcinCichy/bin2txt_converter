deb555d1846c171bf2301d370e0de83fea4e5f63
Marcin Cichy <marcin-cichy@wp.pl>
2023-01-17 03:05:33 +0100

    Added gitignore

--- GUI_bin2txt_conv.py
+++ GUI_bin2txt_conv.py
@@ -8,11 +8,11 @@ class Application(Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.txt_enterField = Text(frm_Second_Frame, width=80, height=25)  # ,width = 100, height = 100
+        self.txt_enterField = Text(width=80, height=25)  # ,width = 100, height = 100
         self.txt_enterField.grid(row=1, column=0, padx=2, pady=10, sticky=NE)
-        scrollbar = Scrollbar(frm_Second_Frame, orient='vertical', command=self.txt_ListOfFiles.yview)
-        scrollbar.grid(row=1, column=2, sticky='NS')
-        self.txt_enterField.config(yscrollcommand=scrollbar.set)
+        # scrollbar = Scrollbar(orient='vertical', command=self.txt_ListOfFiles.yview)
+        # scrollbar.grid(row=1, column=2, sticky='NS')
+        # self.txt_enterField.config(yscrollcommand=scrollbar.set)
 		
 		
 root = Tk()
